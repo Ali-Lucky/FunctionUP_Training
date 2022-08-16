5453021b26db04a95002edf3f14cb8bae2b0ff99
Lucky <luckyshaif888@gmail.com>
2022-08-16 19:05:46 +0530

    Assignment

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-    bookName: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    authorName: String,
-    category: {
-        type: String,
-        enum: ["novel", "biography", "history", "literature"]
-    },
-    year: Number
+    // bookName: {
+    //     type: String,
+    //     unique: true,
+    //     required: true
+    // },
+    // authorName: String,
+    // category: {
+    //     type: String,
+    //     enum: ["novel", "biography", "history", "literature"]
+    // },
+    // year: Number
 }, { timestamps: true });
 
 module.exports = mongoose.model('Book', bookSchema)
