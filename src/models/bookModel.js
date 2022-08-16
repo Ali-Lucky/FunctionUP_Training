58e420c37aaaba74a88d8cee51f99f6f420ecdbf
Lucky <luckyshaif888@gmail.com>
2022-08-16 19:32:02 +0530

    Asignment

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -12,6 +12,25 @@ const bookSchema = new mongoose.Schema({
     //     enum: ["novel", "biography", "history", "literature"]
     // },
     // year: Number
+    bookName: {
+        type: String,
+        required: true
+    },
+    authorName: String, 
+    tags: [String],
+    
+    isPublished: Boolean,
+    prices: {
+        indianPrice: String,
+        europePrice: String,
+    },
+    year: {
+        type: Number,
+        default: 2021
+    },
+    totalPages: Number,
+    stockAvailable: Boolean,
+    sales: {type: Number, default: 10}
 }, { timestamps: true });
 
 module.exports = mongoose.model('Book', bookSchema)
