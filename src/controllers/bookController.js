1e78d743cff55b33aaeb8ee1ad2a095740e1fdda
Lucky <luckyshaif888@gmail.com>
2022-08-16 20:16:04 +0530

    Assignment

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -10,7 +10,9 @@ const getBooksData = async function (req, res) {
     //let allBooks = await BookModel.find()
     //let allBooks = await BookModel.find().select({bookName: 1, authorName: 1, _id: 0})
     //let allBooks = await BookModel.find({year: 2021}).select({bookName: 1, year: 1, _id: 0})
-    let allBooks = await BookModel.find({bookName: /^ab/})
+    // let allBooks = await BookModel.find({bookName: /^ab/})
+    //let allBooks = await BookModel.find({year: 2020})
+    let allBooks = await BookModel.find({})
     res.send({ msg: allBooks })
 }
 
