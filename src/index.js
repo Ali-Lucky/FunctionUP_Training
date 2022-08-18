d8204255677531e77e9eacbf5832ec577b218436
Lucky <luckyshaif888@gmail.com>
2022-08-18 10:50:31 +0530

    Assignment

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Lucky:YUy1kWDQ1NBVgq5y@cluster0.6c2xqpx.mongodb.net/Lucky_04?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+.catch ( err => console.log(err) 
 
 app.use('/', route);
 
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,16 +1,27 @@
 const mongoose = require('mongoose');
 
-
-
-const bookSchema = new mongoose.Schema({
-    name: String,
-    author_id : {
-        type:Number,
-        required: true
+const bookSchema = new mongoose.Schema( {
+    bookName: String, 
+    authorName: String, 
+    tags: [String],
+    
+    isPublished: Boolean,
+    prices: {
+        indianPrice: String,
+        europePrice: String,
     },
-    price : Number,
-    rating : Number
-},
-{timestamps:true})
+    sales: {type: Number, default: 10},
+    
+    // " best boook on earth"   [ "Nodejs in detail" , "mongodb in detail", "fronend in detail"] 
+    // {
+        // "ch1 ": "awesome intro to JS",
+        // "ch2" : "intro to nodejs",
+        // "ch3" : "intro to db"
+    //  }
+    summary :  mongoose.Schema.Types.Mixed,
+    isDeleted: Boolean //true on book deletion i.e you flag the document/data as isDeleted: true..(mark "dirty")
 
-module.exports = mongoose.model('Book1',bookSchema)
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('Book', bookSchema) //users
